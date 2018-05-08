829aeb200eb5bd202209401a6d8370febd38f630
Antonio <ama0114@alu.ubu.es>
2018-05-08 16:46:03 +0200

    Añadir Subclases

---
+++ src/ubu/gii/dass/refactoring/RegularMovie.java
@@ -0,0 +1,10 @@
+package ubu.gii.dass.refactoring;
+
+public class RegularMovie extends Movie{
+
+	public RegularMovie(String title, int priceCode) {
+		super(title, priceCode);
+		// TODO Auto-generated constructor stub
+	}
+
+}
